95fb4691d29866f615f73444d3eea265d70b071a
Breezewish <me@breeswish.org>
2016-03-22 11:32:47 +0800

    Add placeholder for rate limit handler and message request handler

---
+++ Server/src/main/java/octoteam/tahiti/server/pipeline/RateLimitHandler.java
@@ -0,0 +1,19 @@
+package octoteam.tahiti.server.pipeline;
+
+import io.netty.channel.ChannelHandlerContext;
+import octoteam.tahiti.protocol.SocketMessageProtos;
+import octoteam.tahiti.server.TahitiServer;
+
+public class RateLimitHandler extends PipelineMessageHandler {
+
+    public RateLimitHandler(TahitiServer server) {
+        super(server);
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, SocketMessageProtos.Message msg) {
+        // TODO
+        ctx.fireChannelRead(msg);
+    }
+
+}
