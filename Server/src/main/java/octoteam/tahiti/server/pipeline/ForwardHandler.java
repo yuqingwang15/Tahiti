91916c6290d68e1bd8373499af269e2ffe50b14b
justnick <findnick@163.com>
2016-03-23 18:23:31 +0800

    rebuild forward msg and fix flush

--- Server/src/main/java/octoteam/tahiti/server/pipeline/ForwardHandler.java
+++ Server/src/main/java/octoteam/tahiti/server/pipeline/ForwardHandler.java
@@ -1,7 +1,7 @@
 package octoteam.tahiti.server.pipeline;
 
 import io.netty.channel.ChannelHandlerContext;
-import octoteam.tahiti.protocol.SocketMessageProtos;
+import octoteam.tahiti.protocol.SocketMessageProtos.Message;
 import octoteam.tahiti.server.TahitiServer;
 
 /**
@@ -15,7 +15,13 @@ public class ForwardHandler extends PipelineMessageHandler{
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, SocketMessageProtos.Message message) throws Exception {
-        this.server.getAllConnected().write(message);
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Message message) throws Exception {
+        Message.Builder resp = Message
+                .newBuilder()
+                .setSeqId(message.getSeqId())
+                .setDirection(Message.DirectionCode.EVENT)
+                .setService(Message.ServiceCode.CHAT_BROADCAST_EVENT)
+                .setChatMessageReq(message.getChatMessageReq());
+        this.server.getAllConnected().flushAndWrite(resp.build());
     }
 }
