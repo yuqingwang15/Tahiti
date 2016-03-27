46085e3fa4faa32dde208418e91ca4d0f1de661a
Breezewish <me@breeswish.org>
2016-03-27 23:28:49 +0800

    Add UserEventHandlerTest

---
+++ Server/src/test/java/octoteam/tahiti/server/pipeline/UserEventHandlerTest.java
@@ -0,0 +1,51 @@
+package octoteam.tahiti.server.pipeline;
+
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.embedded.EmbeddedChannel;
+import octoteam.tahiti.server.event.BaseEvent;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserEventHandlerTest {
+
+    @Test
+    public void testUserEvent() {
+
+        List<Object> collectedEvents = new LinkedList<>();
+        EventBus eventBus = new EventBus();
+
+        eventBus.register(new Object() {
+            @Subscribe
+            public void collect(DeadEvent ev) {
+                collectedEvents.add(ev.getEvent());
+            }
+        });
+
+        BaseEvent event = new BaseEvent();
+
+        ChannelHandler raiseEventChannelHandler = new ChannelHandlerAdapter() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                super.channelActive(ctx);
+                ctx.fireUserEventTriggered(event);
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(raiseEventChannelHandler, new UserEventHandler(eventBus));
+        channel.finish();
+
+        assertEquals(1, collectedEvents.size());
+        assertEquals(event, collectedEvents.get(0));
+
+    }
+
+}
