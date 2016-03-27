e468e358bad3ab5b8f21dd3237febcd9703bc4a3
Breezewish <me@breeswish.org>
2016-03-27 21:28:05 +0800

    Add MessageRequestHandlerTest

---
+++ Server/src/test/java/octoteam/tahiti/server/pipeline/MessageRequestHandlerTest.java
@@ -0,0 +1,65 @@
+package octoteam.tahiti.server.pipeline;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+import octoteam.tahiti.protocol.SocketMessageProtos.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageRequestHandlerTest {
+
+    @Test
+    public void testMessageRequest() {
+
+        // MessageRequestHandler should handle CHAT_SEND_MESSAGE_REQUEST
+
+        EmbeddedChannel channel = new EmbeddedChannel(new MessageRequestHandler());
+
+        Message msgRequest = Message.newBuilder()
+                .setSeqId(123)
+                .setDirection(Message.DirectionCode.REQUEST)
+                .setService(Message.ServiceCode.CHAT_SEND_MESSAGE_REQUEST)
+                .setChatMessageReq(ChatMessageReqBody.newBuilder()
+                        .setPayload("foo")
+                        .setTimestamp(333)
+                )
+                .build();
+
+        channel.writeInbound(msgRequest);
+        channel.finish();
+
+        assertNull(channel.readInbound());
+
+        Object response = channel.readOutbound();
+        assertTrue(response instanceof Message);
+
+        Message responseMsg = (Message) response;
+        assertTrue(responseMsg.isInitialized());
+        assertEquals(123, responseMsg.getSeqId());
+        assertEquals(Message.DirectionCode.RESPONSE, responseMsg.getDirection());
+        assertEquals(Message.StatusCode.SUCCESS, responseMsg.getStatus());
+
+    }
+
+    @Test
+    public void testOtherRequest() {
+
+        // MessageRequestHandler should not handle other request
+
+        EmbeddedChannel channel = new EmbeddedChannel(new MessageRequestHandler());
+
+        Message otherRequest = Message.newBuilder()
+                .setSeqId(212)
+                .setDirection(Message.DirectionCode.REQUEST)
+                .setService(Message.ServiceCode.PING_REQUEST)
+                .build();
+
+        channel.writeInbound(otherRequest);
+        channel.finish();
+
+        assertEquals(otherRequest, channel.readInbound());
+        assertNull(channel.readOutbound());
+
+    }
+
+}
