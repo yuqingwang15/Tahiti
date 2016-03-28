6655afed80ae05cf2ba307f287f037a2827c13ca
justnick <findnick@163.com>
2016-03-28 18:19:33 +0800

    add SessionExpireHandlerTest

---
+++ Server/src/test/java/octoteam/tahiti/server/pipeline/SessionExpireHandlerTest.java
@@ -0,0 +1,47 @@
+package octoteam.tahiti.server.pipeline;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+import octoteam.tahiti.protocol.SocketMessageProtos.SessionExpiredEventBody;
+import octoteam.tahiti.protocol.SocketMessageProtos.Message;
+import octoteam.tahiti.server.event.RateLimitExceededEvent;
+import octoteam.tahiti.server.ratelimiter.CounterBasedRateLimiter;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SessionExpireHandlerTest {
+
+    /**
+     * clear session once count of messages from client exceeded
+     */
+
+    @Test
+    public void testSessionExpired() {
+        EmbeddedChannel channel = new EmbeddedChannel(new RequestRateLimitHandler(
+                Message.ServiceCode.PING_REQUEST,
+                RateLimitExceededEvent.NAME_PER_SESSION,
+                () -> new CounterBasedRateLimiter(2)), new SessionExpireHandler());
+
+        for (int i = 0; i < 3; i++) {
+            Message pingRequest = Message.newBuilder()
+                    .setSeqId(1)
+                    .setDirection(Message.DirectionCode.REQUEST)
+                    .setService(Message.ServiceCode.PING_REQUEST)
+                    .build();
+            channel.writeInbound(pingRequest);
+            if (i == 2) {
+                // only care about EVENT message
+                Object response = channel.readOutbound();
+                assertTrue(response instanceof Message);
+
+                Message responseMsg = (Message) response;
+                assertEquals(responseMsg.getDirection(), Message.DirectionCode.EVENT);
+                assertEquals(responseMsg.getService(), Message.ServiceCode.SESSION_EXPIRED_EVENT);
+                assertEquals(responseMsg.getSessionExpiredEvent().getReason(),
+                        SessionExpiredEventBody.Reason.EXPIRED);
+            }
+        }
+
+    }
+
+}
