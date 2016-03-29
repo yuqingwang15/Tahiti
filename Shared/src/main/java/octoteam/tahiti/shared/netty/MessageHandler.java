609a9a1c9c6bc02c7e5963ca07b47fb0e986e326
justnick <findnick@163.com>
2016-03-29 22:16:16 +0800

    add javadoc for MessageHandler.java

--- Shared/src/main/java/octoteam/tahiti/shared/netty/MessageHandler.java
+++ Shared/src/main/java/octoteam/tahiti/shared/netty/MessageHandler.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelPromise;
 import octoteam.tahiti.protocol.SocketMessageProtos.Message;
 
 /**
- * TODO
+ * 处理消息（Message）的收发，所有消息处理队列中的 Handler 如果想处理消息类型（Message）的数据都应该继承这个类
  */
 public abstract class MessageHandler extends ChannelDuplexHandler {
 
@@ -20,10 +20,10 @@ public abstract class MessageHandler extends ChannelDuplexHandler {
     }
 
     /**
-     * TODO
+     * 当从外部收到消息是被调用，默认是向消息处理队列中的下一 Handler 传递消息，子类可以通过覆写这个方法实现自定义的消息处理行为
      *
-     * @param ctx
-     * @param msg
+     * @param ctx 当前 Handler 所处的队列环境，可向相同环境内的 Handler 传递消息
+     * @param msg 收到的消息 （Message）
      * @throws Exception
      */
     protected void messageReceived(ChannelHandlerContext ctx, Message msg) throws Exception {
@@ -40,11 +40,11 @@ public abstract class MessageHandler extends ChannelDuplexHandler {
     }
 
     /**
-     * TODO
+     * 当向外部发送消息时被调用，默认是向消息处理队列中的下一 Handler 传递消息，子类可以通过覆写这个方法实现自定义的消息处理行为
      *
-     * @param ctx
-     * @param msg
-     * @param promise
+     * @param ctx 当前 Handler 所处的队列环境，可向相同环境内的 Handler 传递消息
+     * @param msg 发送的消息 （Message）
+     * @param promise 消息传递过程中的数据保证
      * @throws Exception
      */
     protected void messageSent(ChannelHandlerContext ctx, Message msg, ChannelPromise promise) throws Exception {
